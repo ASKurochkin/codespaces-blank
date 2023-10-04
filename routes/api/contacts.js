c60b30d9ebb0073c6dfaad012daea661e6206c8a
OLEKSANDR KUROCHKIN <113820402+ASKurochkin@users.noreply.github.com>
2023-10-04 20:35:24 +0300

    Create project
    Create project

---
+++ routes/api/contacts.js
@@ -0,0 +1,49 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
+
+const { httpError } = require("../../utils");
+
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await listContacts();
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await getContactById(contactId);
+    if (!result) {
+      throw httpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// router.post("/", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+// router.delete("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+// router.put("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+module.exports = router;
