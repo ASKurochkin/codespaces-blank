1e87ef57318e25e5fc9d73100dc6398592defb52
OLEKSANDR KUROCHKIN <113820402+ASKurochkin@users.noreply.github.com>
2023-10-06 06:56:26 +0000

    Add joi

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -10,7 +10,15 @@ const { httpError } = require("../../utils");
 
 const express = require("express");
 
+const Joi = require('joi')
+
 const router = express.Router();
+
+const checkSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+})
 
 router.get("/", async (req, res, next) => {
   try {
@@ -34,9 +42,18 @@ router.get("/:contactId", async (req, res, next) => {
   }
 });
 
-// router.post("/", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
+router.post("/", async (req, res, next) => {
+  try {
+    const result = await addContact(req.body);
+    res.status(201).json(result)
+    if (!result) {
+      throw httpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
 
 // router.delete("/:contactId", async (req, res, next) => {
 //   res.json({ message: "template message" });
